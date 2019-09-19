33ee0cc8931acc6bdec03b3e9d815b37e40c03e4
marijamijailovic <mijailovicmarija@hotmail.com>
2019-09-19 22:53:04 +0200

    Login finish

---
+++ Hubs/MessageHub.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.SignalR;
+
+namespace PoliticsRisk
+{
+    public class MessageHub : Hub
+    {
+        public Task SendMessageToAll(string message)
+        {
+            return Clients.All.SendAsync("ReciveMessage", message);
+        }
+    }
+}
